5dc7f940f087d9ee411f8dbf0050a9d4295b16fa
Sashank <sashank97015@gmail.com>
2023-09-02 08:36:43 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import WeatherInput from './components/WeatherInput';
+import WeatherDisplay from './components/WeatherDisplay';
+import Globe from './components/Globe';
 import './App.css';
 
+
 function App() {
+  const [weatherData, setWeatherData] = useState(null);
+
+  const fetchWeatherData = async (city) => {
+    try {
+      // Use the entered city name in the API URL
+      const apiKey = '90afb922160f62baa77b6bd2dc2b4e70'; // Replace with your API key
+      const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+      
+      // Make the API request using fetch
+      const response = await fetch(apiUrl);
+  
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+  
+      const data = await response.json();
+      // temperatureInCelsius = temperatureInKelvin - 273.15;
+
+  
+      // Update the weatherData state with the fetched data
+      setWeatherData({
+        city: data.name,
+        temperature : (data.main.temp - 273.15).toFixed(2),
+
+        humidity: data.main.humidity,
+        windSpeed: data.wind.speed,
+        condition: data.weather[0].description,
+      });
+    } catch (error) {
+      console.error('Error fetching weather data:', error.message);
+      // Handle errors and set weatherData to null or an error state
+      setWeatherData(null);
+    }
+  };
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Weather Station Dashboard</h1>
+      <WeatherInput onSearch={fetchWeatherData} />
+      <WeatherDisplay weatherData={weatherData} />
+      <Globe />
     </div>
   );
 }
